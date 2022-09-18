a46ed5f140954b0a9593eb960a5d067f76ed2967
Rahul <rahulgore575@gmail.com>
2022-09-18 12:53:05 +0530

    homepage add in package json

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const { parse } = require('url')
 const next = require('next')
 
 const dev = process.env.NODE_ENV !== 'production'
-const hostname = 'localhost'
+const hostname = process.env.NODE_ENV !=="production" ? 'localhost':'Blogs.shreebalajiitraders.com'
 // when using middleware `hostname` and `port` must be provided below
 const app = next({ dev, hostname, port })
 const handle = app.getRequestHandler()
