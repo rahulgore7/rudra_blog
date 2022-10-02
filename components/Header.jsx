fe3732bfedfeb9a324e5e7b2006c603005525fc0
Rahul <rahulgore575@gmail.com>
2022-10-03 02:03:39 +0530

    Update Header.jsx

--- components/Header.jsx
+++ components/Header.jsx
@@ -4,7 +4,7 @@ import Link from 'next/link';
 import { getCategories } from '../services';
 
 const Header = () => {
-  const [categories, setCategories] = useState([]);
+  const [setCategories] = useState([]);
 
   useEffect(() => {
     getCategories().then((newCategories) => {
