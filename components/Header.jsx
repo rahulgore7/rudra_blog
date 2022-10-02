a15a533a809139d050a9af3e82fc6c38f90edba4
Rahul <rahulgore575@gmail.com>
2022-10-03 01:45:19 +0530

    ee

--- components/Header.jsx
+++ components/Header.jsx
@@ -2,17 +2,13 @@ import React, { useState, useEffect } from 'react';
 import Link from 'next/link';
 import Image from 'next/image';
 import { getCategories } from '../services';
-
-
 const Header = () => {
   const [categories, setCategories] = useState([]);
-
   useEffect(() => {
     getCategories().then((newCategories) => {
       setCategories(newCategories);
     });
   }, []);
-
   return (
     <div className="container mx-auto px-10 mb-8">
       <div className="border-b w-full inline-block border-white-400 py-8">
@@ -27,5 +23,4 @@ const Header = () => {
     </div>
   );
 };
-
 export default Header;
