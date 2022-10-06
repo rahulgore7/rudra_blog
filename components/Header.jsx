2e2f3ebb560451e85aee281ba2bd5be51c81bd88
Rahul <rahulgore575@gmail.com>
2022-10-06 21:42:00 +0530

    Revert "ee"
    This reverts commit c37f5ddac193ba9f96cc55670eebe5e242bab979.

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,10 +1,10 @@
 import React, { useState, useEffect } from 'react';
-import Image from 'next/image';
 import Link from 'next/link';
+import Image from 'next/image';
 import { getCategories } from '../services';
 
 const Header = () => {
-  const [categories, setCategories] = useState([]);
+  const [setCategories] = useState([]);
 
   useEffect(() => {
     getCategories().then((newCategories) => {
